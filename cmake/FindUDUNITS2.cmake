cecf052827cb165f14c5e9f52ce845216a8c65fc
Michael Klatt <mdklatt@alumni.ou.edu>
2022-09-12 09:59:05 -0500

    Refactored FindUDUNITS2.cmake

--- cmake/FindUDUNITS2.cmake
+++ cmake/FindUDUNITS2.cmake
@@ -1,34 +1,68 @@
+##
+# Find v2 of the UDUNITS library.
+# <https://www.unidata.ucar.edu/software/udunits/https://www.unidata.ucar.edu/software/udunits>
+#
+# The package exports the UDUNITS2::udunits2 target and defines these standard
+# package variables:
+#   - UDUNITS2_FOUND
+#   - UDUNUITS2_INCLUDE_DIRS
+#   - UDUNITS2_LIBRARIES
+#   - UDUNITS_VERSION (optional)
+#
+# <https://github.com/mdklatt/cmake-module>
+##
+set(package UDUNITS2)
+
+# Attempt to use `pkg-config` first to locate the library in case it provides
+# its own .pc file.
+
 find_package(PkgConfig)
-pkg_check_modules(PC_UDUNITS2 QUIET UDUNITS2)
+if(PKG_CONFIG_FOUND)
+    # In addition to the usual variables, this sets <prefix>_LIBRARY_DIRS,
+    # which is useful in case the library is installed to a non-standard
+    # location.
+    # TODO: Beware, this is untested.
+    message(STATUS "Using pkg-config values for ${package}")
+    pkg_check_modules(${package} QUIET UDUNITS2)
+endif()
 
 
-find_path(UDUNITS2_INCLUDE_DIR
-    NAMES udunits2.h
-    PATHS ${PC_UDUNITS2_INCLUDE_DIRS}
+# Find the library manually.
+
+if(NOT ${package}_FOUND)
+    find_path(${package}_INCLUDE_DIRS NAMES udunits2.h)
+    find_library(${package}_LIBRARIES udunits2)
+endif()
+
+mark_as_advanced(
+    ${package}_FOUND
+    ${package}_INCLUDE_DIRS
+    ${package}_LIBRARIES
+    ${package}_LIBRARY_DIRS
+    ${package}_VERSION
 )
 
-find_library(UDUNITS2_LIBRARY
-    udunits2
-    PATHS ${PC_UDUNITS2_LIBRARIES}
-)
 
-set(UDUNITS2_VERSION ${PC_UDUNITS2_VERSION})
-
-mark_as_advanced(UDUNITS2_FOUND UDUNITS2_INCLUDE_DIR UDUNITS2_VERSION)
+# Set standard package variables.
 
 include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(UDUNITS2
-    REQUIRED_VARS  # set UDUNITS_FOUND only if all these files were found
-        UDUNITS2_INCLUDE_DIR
-        UDUNITS2_LIBRARY
-    VERSION_VAR UDUNITS2_VERSION
+find_package_handle_standard_args(${package}
+    REQUIRED_VARS
+        # Set ${package}_FOUND if all these variables are set.
+        ${package}_INCLUDE_DIRS
+        ${package}_LIBRARIES
+    VERSION_VAR ${package}_VERSION
 )
 
-if(UDUNITS2_FOUND AND NOT TARGET UDUNITS2::udunits2)
-    add_library(UDUNITS2::udunits2 INTERFACE IMPORTED)
-    set_target_properties(UDUNITS2::udunits2 PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${UDUNITS2_INCLUDE_DIR}"
-            INTERFACE_LINK_LIBRARIES "${UDUNITS2_LIBRARY}"
-            )
+# Export package targets.
+
+set(target ${package}::udunits2)
+if(${package}_FOUND AND NOT TARGET ${target})
+    message(STATUS "Defining target ${target}")
+    add_library(${target} INTERFACE IMPORTED)
+    set_target_properties(${target} PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${${package}_INCLUDE_DIRS}"
+        INTERFACE_LINK_LIBRARIES "${${package}_LIBRARIES}"
+        INTERFACE_LIBRARY_DIRS "${${package}_LIBRARY_DIRS}"  # set by pkg_check_modules()
+    )
 endif()
